646868561060c76a861686f1529a2a5c8440dd77
stevenjo5 <stevenjoseph5@yahoo.com>
2026-03-27 23:21:19 +0000

    Day 5: Fixed disk usage parsing and completed self-healing logic

--- check_system.sh
+++ check_system.sh
@@ -8,6 +8,11 @@ USAGE=$(df / | grep / | awk ' {print $5}'| sed 's/%//g' )
 
 if [ "$USAGE" -gt 60 ]; then 
     echo "⚠️  CRITICAL: Disk usage is at $USAGE%!"
+    echo "Cleaning up files older than 7 days..."
+    find . -name "*.txt" -mtime +7 -delete
+    echo "Cleanup complete. Re-checking space..."
+    NEW_USAGE=$(df / | grep / | awk ' {print $5}'| sed 's/%//g' )
+    echo "New Disk Usage: $NEW_USAGE%"
 else 
     echo "✅ SUCCESS: Disk usage is safe at $USAGE%."
 fi 
