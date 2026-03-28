a01ba60268fdd5184f32b28a79a88454c5aa5efd
stevenjo5 <stevenjoseph5@yahoo.com>
2026-03-28 23:24:38 +0000

    Day 6: Finalised modular cleanup function for self-healing logic

--- check_system.sh
+++ check_system.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
+cleanup_logs() {
+    echo "Running Janitor: Deleting old logs..."
+    find . -name "*.log" -type f -delete
+}
 
-DATE=$(date +%Y-%m-%d)
 
-echo "--- System Health Check: $DATE ---"
-
-USAGE=$(df / | grep / | awk ' {print $5}'| sed 's/%//g' )
+USAGE=$(df -P / | grep / | awk ' {print $5}'| sed 's/%//g' )
 
 if [ "$USAGE" -gt 60 ]; then 
     echo "⚠️  CRITICAL: Disk usage is at $USAGE%!"
-    echo "Cleaning up files older than 7 days..."
-    find . -name "*.txt" -mtime +7 -delete
-    echo "Cleanup complete. Re-checking space..."
-    NEW_USAGE=$(df / | grep / | awk ' {print $5}'| sed 's/%//g' )
-    echo "New Disk Usage: $NEW_USAGE%"
+    
+  cleanup_logs
 else 
     echo "✅ SUCCESS: Disk usage is safe at $USAGE%."
 fi 
